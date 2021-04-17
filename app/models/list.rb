a177b4b730b08a426b1b00e0ff25a9e75d944726
Dorothee Wallois <dorothee.wallois@live.fr>
2021-04-17 12:25:41 +0200

    model

---
+++ app/models/list.rb
@@ -0,0 +1,6 @@
+class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, :through => :bookmarks, dependent: :destroy
+  validates_uniqueness_of :name
+  validates_presence_of :name
+end
---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  validates_uniqueness_of :title
+  validates_presence_of :title
+  validates_presence_of :overview
+end
