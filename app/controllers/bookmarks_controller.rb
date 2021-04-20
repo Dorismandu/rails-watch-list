af9cdbc8f3632ea0fdaa298b64c5afe25c92a5a8
Dorothee Wallois <dorothee.wallois@live.fr>
2021-04-20 18:12:08 +0200

    prepare for heroku

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:show, :destroy]
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+    
+  end
+
+  def destroy
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to(@list)
+  end
+
+  private
+  def set_bookmark 
+    @bookmark = Bookmark.find(params[:id])
+  end
+  
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+
+p "destroy all movies"
+Movie.destroy_all
+p "db is clean!"
+p "create movies"
+
+filepath = "http://tmdb.lewagon.com/movie/top_rated"
+
+top_movies = URI.open(filepath).read
+
+movies = JSON.parse(top_movies)
+
+movies["results"].each do |top_movie|
+  Movie.create!(
+    title: top_movie["original_title"],
+    overview: top_movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{top_movie["backdrop_path"]}",
+    rating: top_movie["vote_average"]
+  )
+  p "added a movie"
+end
+
+p "movies created!"
